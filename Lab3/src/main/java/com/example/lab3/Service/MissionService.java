10f24b9fb01d0d169ee71107e4d5e9043cfd8cc9
koooogi <astronaut.kogi@gmail.com>
2026-05-17 14:59:49 +0300

    MissionService

--- Lab3/src/main/java/com/example/lab3/Service/MissionService.java
+++ Lab3/src/main/java/com/example/lab3/Service/MissionService.java
@@ -1,13 +1,44 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.example.lab3.Service;
+
+import com.example.lab3.Model.Mission;
+import com.example.lab3.Repository.MissionRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
  * @author kogi <astronaut.kogi@gmail.com>
  */
+
+@Service
 public class MissionService {
     
+    @Autowired
+    private MissionRepository missionRepository;
+    
+    public Mission saveMission(Mission mission){
+        return missionRepository.save(mission);
+    }
+    
+    public List<Mission> getAllMissions(){
+        return missionRepository.findAll();
+    }
+    
+    public Optional<Mission> getMissionById(Long id){
+        return missionRepository.findById(id);
+    }
+    
+    public Optional<Mission> getMissionByMissionId(String missionId){
+        return missionRepository.findByMissionId(missionId);
+    }
+    
+    public void deleteMission(Long id){
+        missionRepository.deleteById(id);
+    }
+    
+    public boolean existsById(Long id){
+        return missionRepository.existsById(id);
+    }
 }
